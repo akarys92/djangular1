e87a1a88f7d0254d24468199630eddf505d65d63
Alexander Karys <akarys92@Alexander-Karyss-MacBook-Pro.local>
2015-03-16 07:10:01 -0400

    Initial additions

---
+++ thinkster-django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/akarys92/Dev/Djangular/thinkster-django-angular-boilerplate/thinkster-django/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
