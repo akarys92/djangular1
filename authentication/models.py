d70f369ae9ea9c0119d019f24147f9abbdf99df7
Alexander Karys <akarys92@Alexander-Karyss-MacBook-Pro.local>
2015-03-16 07:53:49 -0400

    Updated authentication accounts models

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,31 @@
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
+
+class AccountManager(BaseUserManager):
+	def create_user(self, email, password=None, **kwargs):
+		if not email:
+			raise ValueError('User must have a valid email address')
+		if not kwargs.get('username'):
+			raise ValueError('User must have a valid username')
+
+		account = self.model(
+			email = self.normalize_email(email), username = kwargs.get('username')
+		)
+
+		account.set_password(password)
+		account.save()
+		
+		return account
+
+	def create_superuser(self, email, password, **kwargs):
+		account = self.create_user(email, password, **kwargs)
+		account.is_admin = True
+		account.save()
+
+		return account
+
+		
 
 class Account (AbstractBaseUser):
 	email = models.EmailField(unique=True)
@@ -27,4 +53,3 @@ class Account (AbstractBaseUser):
 
 	def get_short_name(self):
 		return self.first_name
-		
